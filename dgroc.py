86e1ae22321e506ce6f44cf6a8b97e81ca6bb6bb
Pierre-Yves Chibon <pingou@pingoured.fr>
2014-03-20 15:14:22 +0100

    Port to logging instead of using print commands

--- dgroc.py
+++ dgroc.py
@@ -27,6 +27,9 @@ import requests
 
 DEFAULT_CONFIG = os.path.expanduser('~/.config/dgroc')
 COPR_URL = 'http://copr.fedoraproject.org/'
+# Initial simple logging stuff
+logging.basicConfig(format='%(message)s')
+LOG = logging.getLogger("dgroc")
 
 
 class DgrocException(Exception):
@@ -40,6 +43,7 @@ def _get_copr_auth():
     ''' Return the username, login and API token from the copr configuration
     file.
     '''
+    LOG.debug('Reading configuration for copr')
     ## Copr config check
     copr_config_file = os.path.expanduser('~/.config/copr')
     if not os.path.exists(copr_config_file):
@@ -98,7 +102,7 @@ def update_spec(spec_file, commit_hash, archive_name, packager, email):
     ''' Update the release tag and changelog of the specified spec file
     to work with the specified git commit_hash.
     '''
-
+    LOG.debug('Update spec file: %s', spec_file)
     release = '%sgit%s' % (date.today().strftime('%Y%m%d'), commit_hash)
     output = []
     version = None
@@ -110,11 +114,15 @@ def update_spec(spec_file, commit_hash, archive_name, packager, email):
             if row.startswith('Release:'):
                 if commit_hash in row:
                     raise DgrocException('Spec already up to date')
+                LOG.debug('Release line before: %s', row)
                 rel_num = row.split('ase:')[1].strip().split('%{?dist')[0]
                 rel_num = rel_num.split('.')[0]
+                LOG.debug('Release number: %s', rel_num)
                 row = 'Release:        %s.%s%%{?dist}' % (rel_num, release)
+                LOG.debug('Release line after: %s', row)
             if row.startswith('Source0:'):
                 row = 'Source0:        %s' % (archive_name)
+                LOG.debug('Source0 line after: %s', row)
             if row.startswith('%changelog'):
                 output.append(row)
                 output.append('* %s %s <%s> - %s-%s' % (
@@ -129,7 +137,7 @@ def update_spec(spec_file, commit_hash, archive_name, packager, email):
         for row in output:
             stream.write(row + '\n')
 
-    print 'Spec file updated: %s' % spec_file
+    LOG.info('Spec file updated: %s', spec_file)
 
 
 def get_rpm_sourcedir():
@@ -146,7 +154,7 @@ def generate_new_srpm(config, project):
     ''' For a given project in the configuration file generate a new srpm
     if it is possible.
     '''
-
+    LOG.debug('Generating new source rpm for project: %s', project)
     if not config.has_option(project, 'git_folder'):
         raise DgrocException(
             'Project "%s" does not specify a "git_folder" option'
@@ -170,7 +178,7 @@ def generate_new_srpm(config, project):
 
     if not os.path.exists(git_folder):
         git_url = config.get(project, 'git_url')
-        print 'Cloning %s' % git_url
+        LOG.info('Cloning %s', git_url)
         pygit2.clone_repository(git_url, git_folder)
 
     # git pull
@@ -183,15 +191,14 @@ def generate_new_srpm(config, project):
     out = pull.communicate()
     os.chdir(cwd)
     if pull.returncode:
-        print 'Strange result of the git pull:'
-        print out[0]
+        LOG.info('Strange result of the git pull:\n%s', out[0])
         return
 
     # Retrieve last commit
     repo = pygit2.Repository(git_folder)
     commit = repo[repo.head.target]
     commit_hash = commit.oid.hex[:8]
-    print 'last commit: %s -> %s' % (commit.oid.hex, commit_hash)
+    LOG.info('Last commit: %s -> %s', commit.oid.hex, commit_hash)
 
     # Check if commit changed
     changed = False
@@ -212,6 +219,7 @@ def generate_new_srpm(config, project):
     archive_name = "%s-%s.tar" % (project, commit_hash)
     cmd = ["git", "archive", "--format=tar", "--prefix=%s/" % project,
            "-o%s/%s" % (get_rpm_sourcedir(), archive_name), "HEAD"]
+    LOG.debug('Command to generate archive: %s', ' '.join(cmd))
     pull = subprocess.Popen(
         cmd,
         stdout=subprocess.PIPE,
@@ -233,18 +241,18 @@ def generate_new_srpm(config, project):
 
     # Copy patches
     if config.has_option(project, 'patch_files'):
+        LOG.info('Copying patches')
         patches = config.get(project, 'patch_files').split(',')
         patches = [patch.strip() for patch in patches]
-        print patches
         for patch in patches:
+            LOG.debug('Copying patch: %s', patch)
             patch = os.path.expanduser(patch)
-            print patch
             if not patch or not os.path.exists(patch):
-                print '`%s` not found' % patch
+                LOG.info('Patch not found: `%s`', patch)
                 continue
             filename = os.path.basename(patch)
             dest = os.path.join(get_rpm_sourcedir(), filename)
-            print 'Copying from %s, to %s' % (patch, dest)
+            LOG.debug('Copying from %s, to %s', patch, dest)
             shutil.copy(
                 patch,
                 dest
@@ -258,12 +266,14 @@ def generate_new_srpm(config, project):
     out = build.communicate()
     os.chdir(cwd)
     if build.returncode:
-        print 'Strange result of the rpmbuild -bs:'
-        print out[0]
-        print out[1]
+        LOG.info(
+            'Strange result of the rpmbuild -bs:\n  stdout:%s\n  stderr:%s',
+            out[0],
+            out[1]
+        )
         return
     srpm = out[0].split('Wrote:')[1].strip()
-    print 'SRPM built: %s' % srpm
+    LOG.info('SRPM built: %s', srpm)
 
     return srpm
 
@@ -280,11 +290,11 @@ def upload_srpms(config, srpms):
     upload_command = config.get('main', 'upload_command')
 
     for srpm in srpms:
+        LOG.debug('Uploading source rpm: %s', srpm)
         cmd = upload_command % srpm
         outcode = subprocess.call(cmd, shell=True)
         if outcode:
-            print 'Strange result with the command:'
-            print cmd
+            LOG.info('Strange result with the command: `%s`', ' '.join(cmd))
 
 
 def copr_build(config, srpms):
@@ -340,23 +350,23 @@ def copr_build(config, srpms):
             URL, auth=(login, token), data=data, verify=not insecure)
 
         if '<title>Sign in Coprs</title>' in req.text:
-            print "Invalid API token"
+            LOG.info("Invalid API token")
             return
 
         if req.status_code == 404:
-            print "Project %s/%s not found." % (user['username'], project)
+            LOG.info("Project %s/%s not found.", user['username'], project)
 
         try:
-            output = json.loads(req.text)
+            output = json.loads(req.txt)
         except ValueError:
-            print "Unknown response from server."
-            print req.text
-            print req.json()
+            LOG.info("Unknown response from server.")
+            LOG.debug(req.url)
+            LOG.debug(req.text)
             return
         if req.status_code != 200:
-            print "Something went wrong:\n  %s" % (output['error'])
+            LOG.info("Something went wrong:\n  %s", output['error'])
             return
-        print output
+        LOG.info(output)
         build_ids.append(output['id'])
     return build_ids
 
@@ -399,22 +409,23 @@ def check_copr_build(config, build_ids):
             URL, auth=(login, token), verify=not insecure)
 
         if '<title>Sign in Coprs</title>' in req.text:
-            print "Invalid API token"
+            LOG.info("Invalid API token")
             return
 
         if req.status_code == 404:
-            print "Build %s not found." % (build_id)
+            LOG.info("Build %s not found.", build_id)
 
         try:
             output = json.loads(req.text)
         except ValueError:
-            print "Unknown response from server."
-            print req.text
+            LOG.info("Unknown response from server.")
+            LOG.debug(req.url)
+            LOG.debug(req.text)
             return
         if req.status_code != 200:
-            print "Something went wrong:\n  %s" % (output['error'])
+            LOG.info("Something went wrong:\n  %s", output['error'])
             return
-        print '  Build %s' % build_id, output
+        LOG.info('  Build %s: %s', build_id, output)
 
         if output['status'] in ('pending', 'running'):
             build_ip.append(build_id)
@@ -426,6 +437,13 @@ def main():
     '''
     # Retrieve arguments
     args = get_arguments()
+
+    global LOG
+    #global LOG
+    if args.debug:
+        LOG.setLevel(logging.DEBUG)
+    else:
+        LOG.setLevel(logging.INFO)
 
     # Read configuration file
     config = ConfigParser.ConfigParser()
@@ -445,12 +463,13 @@ def main():
     for project in config.sections():
         if project == 'main':
             continue
+        LOG.info('Processing project: %s', project)
         try:
             srpms[project] = generate_new_srpm(config, project)
         except DgrocException, err:
-            print '%s: %s' % (project, err)
+            LOG.info('%s: %s', project, err)
 
-    print '%s srpms generated' % len(srpms)
+    LOG.info('%s srpms generated', len(srpms))
     if not srpms:
         return
 
@@ -460,18 +479,18 @@ def main():
     try:
         upload_srpms(config, srpms.values())
     except DgrocException, err:
-        print err
+        LOG.info(err)
 
     try:
         build_ids = copr_build(config, srpms)
     except DgrocException, err:
-        print err
+        LOG.info(err)
 
     if args.monitoring:
-        print 'Monitoring %s builds...' % len(build_ids)
+        LOG.info('Monitoring %s builds...', len(build_ids))
         while build_ids:
             time.sleep(45)
-            print datetime.datetime.now()
+            LOG.info(datetime.datetime.now())
             build_ids = check_copr_build(config, build_ids)
 
 
